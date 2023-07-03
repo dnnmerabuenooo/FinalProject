b01e7196b0a75131ccd4dc70fdede703d780b53b
Dianne Merabueno <111546446+dnnmerabuenooo@users.noreply.github.com>
2023-07-04 00:48:47 +0800

    Update UserInfo.java

--- UserInfo.java
+++ UserInfo.java
@@ -1,73 +1,55 @@
-import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.ImageIcon;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
-public class UserPage extends JFrame {
+public class UserInfo {
+    private String name;
+    private String address;
+    private String phone;
 
-	private JPanel contentPane;
+    public UserInfo(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					UserPage frame = new UserPage();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    public void saveToFile() {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("Information.txt", true));
+            writer.write(name + " , " + address + " , " + phone);
+            writer.newLine();
+            writer.close();
 
-	/**
-	 * Create the frame.
-	 */
-	public UserPage() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1000, 600);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+            System.out.println("Successfully Logged In");
+        } catch (IOException ex) {
+            System.out.println("Failed to write information to file");
+            ex.printStackTrace();
+        }
+    }
 
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JLabel userpagePic = new JLabel("");
-		userpagePic.setIcon(new ImageIcon("C:\\Users\\callv\\OneDrive\\Pictures\\Project\\UserPage.png"));
-		userpagePic.setBounds(0, 0, 984, 561);
-		contentPane.add(userpagePic);
-		
-		JLabel vieweqBtn = new JLabel("");
-		vieweqBtn.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				ViewEquipment viewEquipment = new ViewEquipment();
-				viewEquipment.setVisible(true);
-				dispose();
-			}
-		});
-		vieweqBtn.setBounds(274, 280, 185, 52);
-		contentPane.add(vieweqBtn);
-		
-		JLabel rentBtn = new JLabel("");
-		rentBtn.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				Rent rent = new Rent();
-				rent.setVisible(true);
-				dispose();
-			}
-		});
-		rentBtn.setBounds(536, 280, 191, 52);
-		contentPane.add(rentBtn);
-	}
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
